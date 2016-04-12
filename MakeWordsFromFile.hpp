c2d18e396381bd9eb8296aa3096b2b0ee58f551c
chgibb <chris.gibb@outlook.com>
2016-04-12 00:04:49 -0400

    First Commit

---
+++ MakeWordsFromFile.hpp
@@ -0,0 +1,62 @@
+/*
+	Author: Chris Gibb
+    Contact: cgibb@lakeheadu.ca
+*/
+#pragma once
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+//parse file identified by string filename into individual words
+//held in tokenlist
+bool MakeWordsFromFile(std::string filename,std::vector<std::string>&tokenlist,bool lowercase = true)
+{
+
+    std::ifstream file(filename.c_str(),std::ios::in);
+    char byte;
+    std::string str;
+    bool add = true;
+    if(file.fail())
+	{
+		return false;
+	}
+    while(file.get(byte))
+    {
+        add = true;
+        switch(byte)
+        {
+            case ' ':
+                //cull common punctuation and formating characters
+                str.erase(std::remove(str.begin(),str.end(),'\n'),str.end());
+                str.erase(std::remove(str.begin(),str.end(),'.'),str.end());
+                str.erase(std::remove(str.begin(),str.end(),','),str.end());
+                str.erase(std::remove(str.begin(),str.end(),'\t'),str.end());
+                //make everything lowercase
+                if(lowercase)
+                    std::transform(str.begin(),str.end(),str.begin(),::tolower);
+                if(str != "")
+                tokenlist.push_back(str);
+                str = "";
+                add = false;
+            break;
+            case '\n':
+                //cull common punctuation and formating characters
+                str.erase(std::remove(str.begin(),str.end(),'\n'),str.end());
+                str.erase(std::remove(str.begin(),str.end(),'.'),str.end());
+                str.erase(std::remove(str.begin(),str.end(),','),str.end());
+                str.erase(std::remove(str.begin(),str.end(),'\t'),str.end());
+                //make everything lowercase
+                if(lowercase)
+                    std::transform(str.begin(),str.end(),str.begin(),::tolower);
+                if(str != "")
+                tokenlist.push_back(str);
+                str = "";
+                add = false;
+            break;
+        }
+        if(add)
+            str += byte;
+    }
+	file.close();
+    return true;
+}
---
+++ main.cpp
@@ -0,0 +1,90 @@
+/*
+	Author: Chris Gibb
+    Contact: cgibb@lakeheadu.ca
+
+    Simple command line utility to transfer data and check for transfer integrity.
+
+    -copy src to dest
+        cgdt src dest
+    -compare size of src to size of dest (without any copying)
+        cgdt src dest -check
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include "MakeWordsFromFile.hpp"
+using namespace std;
+//parse directory listing generated from dir
+int GetTotalBytesFromDirListing(std::vector<std::string>&dump)
+{
+    auto end = dump.end();
+    for(auto iter = dump.begin(); iter != end; ++iter)
+    {
+        //at the end of the output there will be something like
+        //"Total Files Listed:\n
+        //xxx File(s)  x,xxx,xxx bytes"
+        //Search for this pattern and extract the total bytes in the listing
+        if(*iter == "Total")
+        {
+            ++iter;
+            if(*iter == "Files")
+            {
+                ++iter;
+                if(*iter == "Listed:")
+                {
+                    ++iter;
+                    ++iter;
+                    ++iter;
+                    return std::atoi((*iter).c_str());
+                }
+            }
+        }
+    }
+    return 0;
+}
+int main(int argc,char* argv[])
+{
+    //only check size of source vs destination, do not copy anything
+    bool checkonly = false;
+    std::vector<std::string> args;
+    for(int i = 0; i != argc + 1; i++)
+    {
+        if(argv[i])
+            args.push_back(std::string(argv[i]));
+    }
+    for(auto iter = args.begin(); iter != args.end(); ++iter)
+    {
+        if(*iter == "-check")
+            checkonly = true;
+    }
+    system("rmdir /S /Q tmp");
+    system("mkdir tmp");
+    if(!checkonly)
+        system(std::string("robocopy " + args[1] + " " + args[2]+ " /E /R:1 /W:1").c_str());
+
+    //get directory listing of source and destination
+    system(std::string("dir /S "+args[1]+" > tmp/src.txt").c_str());
+    system(std::string("dir /S "+args[2]+" > tmp/dest.txt").c_str());
+
+    std::vector<std::string> src;
+    std::vector<std::string> dest;
+    ::MakeWordsFromFile("tmp/src.txt",src,false);
+    ::MakeWordsFromFile("tmp/dest.txt",dest,false);
+
+    std::cout<<"\n\n\n";
+    int srcsize = ::GetTotalBytesFromDirListing(src);
+    int destsize = ::GetTotalBytesFromDirListing(dest);
+
+    if(srcsize != destsize)
+        std::cout<<"Error in copy. Source and destination sizes do not match.\n";
+    if(srcsize == destsize)
+        std::cout<<"Success. Total Size In Bytes: "<<srcsize<<"\n";
+    if(srcsize > destsize)
+        std::cout<<"Error. Source is Larger By "<<srcsize-destsize<<" bytes.\n";
+    if(srcsize < destsize)
+        std::cout<<"Error. Source is Smaller By "<<destsize-srcsize<<" bytes.\n";
+
+    system("rmdir /S /Q tmp");
+    return 0;
+}
